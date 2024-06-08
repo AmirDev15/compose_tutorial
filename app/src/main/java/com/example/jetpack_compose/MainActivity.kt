7340162fc56ed77e371df30e43a8ec7cddb2bd7d
Amir <amirfadhil.abd@gmail.com>
2024-06-08 11:34:03 +0300

    checking git diff

--- app/src/main/java/com/example/jetpack_compose/MainActivity.kt
+++ app/src/main/java/com/example/jetpack_compose/MainActivity.kt
@@ -203,7 +203,10 @@ class MainActivity : ComponentActivity() {
         setContent {
 
            // DropdownMenu()
-Text(text = "welcome to github")
+Text(modifier = Modifier,
+    text = "welcome to github")
+
+
 
         }
     }
